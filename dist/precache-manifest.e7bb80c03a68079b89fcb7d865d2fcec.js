5fa7a9185c549e8b60993689ab116fb9fe638920
TXG-Ryan <ryan.brown@txg.ie>
2022-03-10 17:17:14 +0000

    commit /dist/

---
+++ dist/precache-manifest.e7bb80c03a68079b89fcb7d865d2fcec.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f25d2797173743cd1cfe",
+    "url": "/css/app.ed7c1204.css"
+  },
+  {
+    "revision": "2b8a77ba81951c614c2e77589f06a83f",
+    "url": "/img/DATAx.2b8a77ba.svg"
+  },
+  {
+    "revision": "ff12b540fafc31d5e53e2e9dd73942ca",
+    "url": "/img/arrow-down-right.ff12b540.svg"
+  },
+  {
+    "revision": "0356d07db2af76507cad8e9766758085",
+    "url": "/img/arrow-up-right.0356d07d.svg"
+  },
+  {
+    "revision": "edae64fcb50f425e582a9a1429784b70",
+    "url": "/img/btc.edae64fc.svg"
+  },
+  {
+    "revision": "72aeb4693abd84dbd3cad8055d0e2e42",
+    "url": "/img/dd-arrow-dblue.72aeb469.svg"
+  },
+  {
+    "revision": "a01642910a150e0ea6fed59c631443fb",
+    "url": "/img/decrease-icon.a0164291.svg"
+  },
+  {
+    "revision": "45a59663e58bf0610b28632e565b698c",
+    "url": "/img/increase-icon.45a59663.svg"
+  },
+  {
+    "revision": "0329d8a630c185f02833493f3e038770",
+    "url": "/img/search.0329d8a6.svg"
+  },
+  {
+    "revision": "0c735e766e34e6e69661028d2ccc78a3",
+    "url": "/img/sponsored.0c735e76.png"
+  },
+  {
+    "revision": "3a1774b722709196b45dba33a3d649ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25d2797173743cd1cfe",
+    "url": "/js/app.34c82e2b.js"
+  },
+  {
+    "revision": "78b8253ecef7a1f3bcad",
+    "url": "/js/chunk-vendors.42200277.js"
+  },
+  {
+    "revision": "f87f9c4857e2d87106ae8cdaf7619d7a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "afb4942e5838a7dc3b63d00ffcc87c9d",
+    "url": "/normalize.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "690c84f64c05d70f187cbee7542f7382",
+    "url": "/webflow.css"
+  }
+]);
