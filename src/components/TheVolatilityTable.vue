dcb6c6dba082068cc6c92fd0b6d03b3d15e84837
TXG-Ryan <ryan.brown@txg.ie>
2022-03-10 17:43:40 +0000

    Update TheVolatilityTable.vue

--- src/components/TheVolatilityTable.vue
+++ src/components/TheVolatilityTable.vue
@@ -5,7 +5,7 @@
           <div class="table-toolbar">
             <div class="pt-wrapper">
               <div class="pt-left">
-                <div class="text-block-3">Cryptocurrencies Sorted by Volatility Ascending</div>
+                <div class="text-block-3">Cryptocurrencies Sorted by Volatility</div>
               </div>
               <div class="pt-right">
                 <div class="category-selectors">
@@ -72,17 +72,17 @@
                 </div>
                 <div class="cell l">
                   <div class="change-wrap">
-                    <div :class="percentValueStyler(value.price_change_percentage_1h_in_currency)">{{ parseFloat( value.price_change_percentage_1h_in_currency?.toLocaleString() ).toFixed(2) }}%</div>
-                  </div>
-                </div>
-                <div class="cell l">
-                  <div class="change-wrap">
                     <div :class="percentValueStyler(value.price_change_percentage_24h_in_currency)">{{ parseFloat( value.price_change_percentage_24h_in_currency?.toLocaleString() ).toFixed(2) }}%</div>
                   </div>
                 </div>
                 <div class="cell l">
                   <div class="change-wrap">
-                    <div :class="percentValueStyler(value.price_change_percentage_24h_in_currency)">{{ parseFloat( value.price_change_percentage_24h_in_currency?.toLocaleString() ).toFixed(2) }}%</div>
+                    <div :class="percentValueStyler(value.price_change_percentage_7d_in_currency)">{{ parseFloat( value.price_change_percentage_7d_in_currency?.toLocaleString() ).toFixed(2) }}%</div>
+                  </div>
+                </div>
+                <div class="cell l">
+                  <div class="change-wrap">
+                    <div :class="percentValueStyler(value.price_change_percentage_30d_in_currency)">{{ parseFloat( value.price_change_percentage_30d_in_currency?.toLocaleString() ).toFixed(2) }}%</div>
                   </div>
                 </div>
                  <div class="cell l">
@@ -149,9 +149,11 @@ export default {
         },
 
       percentValueStyler(value) { return {
-          'positivePct': value > 0 && value <= 25, 'positivePctLarge': value > 25,
-          'negativePct': value < 0 && value >= -25, 'negativePctLarge': value < 25,
-          'neutralPct': value = 0,
+          'positivePct': value > 0 && value <= 25, 
+          'positivePctLarge': value > 25,
+
+          'negativePct': value < 0 && value >= -25, 
+          'negativePctLarge': value < -25,
         };
       },
 
@@ -160,10 +162,6 @@ export default {
           'mediumVol': value > 25 && value <= 50,
           'highVol': value > 50 && value <= 75,
           'extremeVol': value > 75 && value <= 100,
-
-
-          'negativePct': value < 0 && value >= -25, 'negativePctLarge': value < 25,
-          'neutralPct': value = 0,
         };
       },
 
@@ -311,6 +309,31 @@ export default {
   color: #d9475a;
   font-weight: 600;
 }
+
+.negativePctLarge {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  height: 27px;
+  padding-right: 18px;
+  -webkit-box-pack: end;
+  -webkit-justify-content: flex-end;
+  -ms-flex-pack: end;
+  justify-content: flex-end;
+  -webkit-box-align: center;
+  -webkit-align-items: center;
+  -ms-flex-align: center;
+  align-items: center;
+  border-radius: 4px;
+  background-image: url('../assets/images/arrow-down-right.svg');
+  background-position: 100% 50%;
+  background-size: 13px;
+  background-repeat: no-repeat;
+  color: #d9475a;
+  font-weight: 800;
+}
+
 
 
 
